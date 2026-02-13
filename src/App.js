ac605a340de86327c2a82676c505e1aeef0053c4
vijay-dev07 <uk.kaithwas771@gmail.com>
2026-02-13 16:53:53 +0530

    initial setup

--- src/App.js
+++ src/App.js
@@ -1,24 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import { Provider } from "react-redux";
+import "./App.css";
+import Body from "./components/Body";
+import Head from "./components/Head";
+import store from "./utls/store";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <Provider store={store}>
+    <div>
+        <h1 className="text-3xl font-bold underline"> </h1>
+        <Head/>
+        <Body/>
+
+     
     </div>
+    </Provider>
   );
 }
 
